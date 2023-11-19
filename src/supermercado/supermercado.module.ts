b97346ba14a951f1735c393662fc6b6162e8b3b5
SaragRam <sg.ramirez940@uniandes.edu.co>
2023-11-18 23:52:09 -0500

    ajuste módulo supermercado

--- src/supermercado/supermercado.module.ts
+++ src/supermercado/supermercado.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SupermercadoEntity } from './supermercado.entity';
 import { SupermercadoService } from './supermercado.service';
 import { SupermercadoController } from './supermercado.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([SupermercadoEntity])],
   providers: [SupermercadoService],
   controllers: [SupermercadoController]
 })
